7e94e7f6d73f026d73118becda4d3cb9cf9e0d45
Ramon Bartl <rb@ridingbytes.com>
2017-09-29 12:54:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+  <h1>
+    <a href="https://github.com/senaite/senaite.jsonapi">
+      <div>
+        <img src="static/logo.png" alt="senaite.jsonapi" />
+      </div>
+    </a>
+  </h1>
+
+  <p>The JSON API provides a RESTful JSON interface to SENAITE LIMS.</p>
+
+
+  <div>
+    <a href="https://travis-ci.org/senaite/senaite.api">
+      <img src="https://img.shields.io/travis/senaite/senaite.jsonapi.svg?style=flat-square" alt="travis-ci" />
+    </a>
+    <a href="docs/Contributing.rst">
+      <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square" alt="prs-welcom" />
+    </a>
+  </div>
+</div>
---
+++ setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+
+setup(
+    name='senaite.jsonapi',
+    version=version,
+    description="SENAITE JSON API",
+    long_description=open("README.md").read() + "\n" +
+                     open("docs/Changelog.rst").read() + "\n" +
+                     "\n\n" +
+                     "Authors and maintainers\n" +
+                     "-----------------------\n" +
+                     "- RIDING BYTES, http://ridingbytes.com",
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    keywords='',
+    author='SENAITE Foundation',
+    author_email='hello@senaite.com',
+    url='https://github.com/senaite/senaite.jsonapi',
+    license='GPLv3',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['senaite'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.api',
+        'senaite.api',
+        'bika.lims>=3.2.1rc3',
+    ],
+    extras_require={
+        'test': [
+            'Products.PloneTestCase',
+            'Products.SecureMailHost',
+            'plone.app.robotframework',
+            'plone.app.testing',
+            'robotframework-debuglibrary',
+            'robotframework-selenium2library',
+            'robotsuite',
+            'unittest2',
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
