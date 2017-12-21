8f72e5b4ca441f7684785bd3e0623b61d3b1185e
Juan Gallostra <jgallostra@naralabs.com>
2017-12-21 10:14:26 +0100

    Modify route, accept a keyword parameter and make use of get_batch api method to prepare results

--- src/senaite/jsonapi/v1/routes/registry.py
+++ src/senaite/jsonapi/v1/routes/registry.py
@@ -5,26 +5,17 @@ from senaite.jsonapi import request as req
 from senaite.jsonapi.v1 import add_route
 
 
-@add_route("/senaite-config-registry", "senaite.jsonapi.v1.senaite-config-registry", methods=["GET"])
-@add_route("/registry/senaite-config-registry", "senaite.jsonapi.v1.registry.senaite-config-registry", methods=["GET"])
-def get(context, request, username=None):
-    """Senaite configuration registry records route
+@add_route("/registry", "senaite.jsonapi.v1.registry", methods=["GET"])
+@add_route("/registry/<string:key>", "senaite.jsonapi.v1.registry", methods=["GET"])
+def get(context, request, key=None):
+    """Return all registry items if key is None, otherwise try to fetch the registry key
     """
-    registry_records = api.get_registry_records_by_keyword("bika")
-    registry_records.update(api.get_registry_records_by_keyword("senaite"))
+    registry_records = api.get_registry_records_by_keyword(key)
 
     # Prepare batch
     size = req.get_batch_size()
     start = req.get_batch_start()
-    batch = api.make_batch(registry_records, size, start)
+    batch = api.get_batch(registry_records, size, start)
+    batch['url'] = api.url_for("senaite.jsonapi.v1.registry", keyword=key)
 
-    return {
-        "pagesize": batch.get_pagesize(),
-        "next": batch.make_next_url(),
-        "previous": batch.make_prev_url(),
-        "page": batch.get_pagenumber(),
-        "pages": batch.get_numpages(),
-        "count": batch.get_sequence_length(),
-        "items": registry_records,
-        "url": api.url_for("senaite.jsonapi.v1.senaite-config-registry"),
-    }
+    return batch
