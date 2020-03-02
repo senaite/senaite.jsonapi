9696fd4a5d07774eada485ea305efc586ff36540
Ramon Bartl <rb@ridingbytes.com>
2020-03-02 18:45:12 +0100

    Updated setup.py and version

--- setup.py
+++ setup.py
@@ -2,52 +2,55 @@
 
 from setuptools import setup, find_packages
 
-version = '1.2.1'
+version = "1.2.1"
 
+with open("docs/About.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("src/senaite/jsonapi/docs/JSONAPIv1.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += fh.read()
+
+with open("docs/Changelog.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += "Changelog\n"
+    long_description += "=========\n\n"
+    long_description += fh.read()
 
 setup(
-    name='senaite.jsonapi',
+    name="senaite.jsonapi",
     version=version,
     description="SENAITE JSON API",
-    long_description=open("docs/About.rst").read() +
-                     "\n\n" +
-                     open("src/senaite/jsonapi/docs/JSONAPIv1.rst").read() +
-                     "\n\n" +
-                     "Changelog\n" +
-                     "=========\n" +
-                     open("docs/Changelog.rst").read() + "\n",
+    long_description=long_description,
     # Get more strings from
     # http://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
-        "Programming Language :: Python",
         "Framework :: Plone",
         "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='',
-    author='SENAITE Foundation',
-    author_email='hello@senaite.com',
-    url='https://github.com/senaite/senaite.jsonapi',
-    license='GPLv2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    package_dir={'': 'src'},
-    namespace_packages=['senaite'],
+    keywords="",
+    author="RIDING BYTES & NARALABS",
+    author_email="senaite@senaite.com",
+    url="https://github.com/senaite/senaite.jsonapi",
+    license="GPLv2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    package_dir={"": "src"},
+    namespace_packages=["senaite"],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools',
-        'senaite.api',
-        'senaite.core',
+        "setuptools",
+        "senaite.core",
     ],
     extras_require={
-        'test': [
-            'Products.PloneTestCase',
-            'Products.SecureMailHost',
-            'plone.app.robotframework',
-            'plone.app.testing',
-            'robotframework-debuglibrary',
-            'robotframework-selenium2library',
-            'robotsuite',
-            'unittest2',
+        "test": [
+            "Products.PloneTestCase",
+            "Products.SecureMailHost",
+            "plone.app.testing",
+            "unittest2",
         ]
     },
     entry_points="""
