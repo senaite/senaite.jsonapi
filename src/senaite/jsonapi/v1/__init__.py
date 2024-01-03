888f750fec75f0a8ee311d750cd076225830e759
Ramon Bartl <rb@ridingbytes.com>
2024-01-03 17:08:15 +0100

    Development is now on 2.6.0

--- src/senaite/jsonapi/v1/__init__.py
+++ src/senaite/jsonapi/v1/__init__.py
@@ -24,8 +24,8 @@ from senaite.jsonapi import logger
 from senaite.jsonapi.v1 import routes
 from senaite.jsonapi import add_route as add_senaite_route
 
-__version__ = "2.5.0"
-__date__ = "2024-01-03"
+__version__ = "2.6.0"
+__date__ = "unreleased"
 
 BASE_URL = "/senaite/v1"
 
