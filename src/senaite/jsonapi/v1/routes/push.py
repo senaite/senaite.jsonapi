f7ee57125d11dfad615f4f97fc8df4d7b92b126a
Jordi Puiggené <jp@naralabs.com>
2025-02-23 22:04:52 +0100

    Update code headers

--- src/senaite/jsonapi/v1/routes/push.py
+++ src/senaite/jsonapi/v1/routes/push.py
@@ -2,9 +2,9 @@
 #
 # This file is part of SENAITE.JSONAPI.
 #
-# SENAITE.JSONAPI is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, version 2.
+# SENAITE.JSONAPI is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
